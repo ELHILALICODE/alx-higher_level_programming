63ec5d01072dc47007e9fd54145cce4949242645
ELHILALICODE <133546427+ELHILALICODE@users.noreply.github.com>
2024-07-30 12:45:57 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
+// Star Wars
 
 const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, (error, response, body) => {
-  if (error) {}
-  const name = JSON.parse(body).title;
-  console.log(name);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    console.log(content.title);
+  }
 });
